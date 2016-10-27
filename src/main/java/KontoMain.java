9984a3a1f1feb2b74d2c8f5c1045b94141856335
Domi <zlotowska.dominika@gmail.com>
2016-10-27 16:45:14 +0200

    NowyBankowy_6 do poprawy plik z oprocentowaniem!

--- src/main/java/KontoMain.java
+++ src/main/java/KontoMain.java
@@ -5,16 +5,22 @@ public class KontoMain {
         Konto kontoAliny = new Konto("111111111", 13000, "Kowalska");
         Konto kontoKarola = new Konto("222222222", 12, "Kowalski");
 
-        System.out.println("Saldo początkowe Aliny i Karola: \n konto Aliny: " + kontoAliny.saldo +
-        "\n konto Karola: " + kontoKarola.saldo +"\n");
+        KontoOprocentowane kontoAsi = new KontoOprocentowane("111111111", 13000, "Kowalska");
+        KontoOprocentowane kontoJarka = new KontoOprocentowane("222222222", 150000, "Kowalski");
 
-        kontoAliny.przelewNa(1000, kontoKarola);
-        kontoKarola.wpłata(5000);
-        kontoKarola.przelewNa(1000, kontoKarola);
+//        System.out.println("Saldo początkowe Aliny i Karola: \n konto Aliny: " + kontoAliny.saldo +
+//        "\n konto Karola: " + kontoKarola.saldo +"\n");
+//
+//        kontoAliny.przelewNa(1000, kontoKarola);
+//        kontoKarola.wpłata(5000);
+//        kontoKarola.przelewNa(1000, kontoKarola);
+//
+//        System.out.println("\n" + "Saldo końcowe Aliny i Karola: \n konto Aliny: " + kontoAliny.saldo +
+//                "\n konto Karola: " + kontoKarola.saldo + "\n");
 
-        System.out.println("\n" + "Saldo końcowe Aliny i Karola: \n konto Aliny: " + kontoAliny.saldo +
-                "\n konto Karola: " + kontoKarola.saldo + "\n");
+//        System.out.println(kontoAliny.toString());
 
-        System.out.println(kontoAliny.toString());
+
+        System.out.println(kontoJarka.wyliczOdsetki(10));
     }
 }
---
+++ src/main/java/KontoOprocentowane.java
@@ -0,0 +1,48 @@
+
+public class KontoOprocentowane {
+
+    String numerKonta;
+    int saldo;
+    String właścicielNazwisko;
+    int odsetki;
+    int oprocentowanie;
+
+    public KontoOprocentowane(String numerKonta, int saldo, String właścicielNazwisko) {
+        this.numerKonta = numerKonta;
+        this.saldo = saldo;
+        this.właścicielNazwisko = właścicielNazwisko;
+    }
+
+    public KontoOprocentowane(String numerKonta0, String właścicielNazwisko0) {
+        this.numerKonta = numerKonta0;
+        this.właścicielNazwisko = właścicielNazwisko0;
+        saldo = 0;
+    }
+
+    public void wpłata(int kwotaWpłaty) {
+        saldo += kwotaWpłaty;
+    }
+
+    public void przelewNa(int kwotaPrzelewu, KontoOprocentowane kontoOdbiorcy) {
+
+        if (this != kontoOdbiorcy) {
+            kontoOdbiorcy.saldo = kontoOdbiorcy.saldo + kwotaPrzelewu;
+            this.saldo = this.saldo - kwotaPrzelewu;
+        } else {
+            System.out.println("Nie możesz przelać pieniędzy na własne konto!");
+        }
+    }
+
+    public int wyliczOdsetki (int oprocentowanie) {
+        oprocentowanie = this.oprocentowanie;
+        saldo = this.saldo;
+        odsetki = (saldo * (oprocentowanie/100));
+        return saldo += odsetki;
+        }
+
+
+    public String toString () {
+        return "Właścicielem konta jest Pan/i " + właścicielNazwisko + ". Numer konta to: " + numerKonta +
+                ", a stan środków na koncie wynosi: " + saldo + "." + "\n Osetki wynoszą: " + odsetki;
+    }
+}
