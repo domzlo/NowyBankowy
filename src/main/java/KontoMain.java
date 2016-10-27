868c037e406b6402425752bec5e4b114860a2532
Domi <zlotowska.dominika@gmail.com>
2016-10-27 14:03:51 +0200

    NowyBankowy_2

--- src/main/java/KontoMain.java
+++ src/main/java/KontoMain.java
@@ -3,10 +3,10 @@ public class KontoMain {
     public static void main(String[] args) {
 
         Konto kontoAliny = new Konto("111111111", 13000, "Kowalska");
-        Konto kontoKarola = new Konto("222222222", "Kowalski");
+        Konto kontoKarola = new Konto("222222222", 12, "Kowalski");
 
         kontoAliny.przelewNa(1000, kontoAliny, kontoKarola);
-//        kontoKarola.wpłata(5000);
+        kontoKarola.wpłata(5000);
 
 
         System.out.println(kontoAliny.saldo);
